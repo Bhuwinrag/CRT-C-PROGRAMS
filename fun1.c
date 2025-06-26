6d24ffa5d428874e37f297c86eb424165ad1fee6
Bhuwin <149753612+Bhuwinrag@users.noreply.github.com>
2025-06-26 14:42:22 +0530

    basic function example

---
+++ fun1.c
@@ -0,0 +1,10 @@
+/*Write a program to inform whether the user exexution is started */
+#include<stdio.h> 
+int main()
+{
+    greet();  //function declaration
+}
+
+void greet(){
+    printf("Execution is started");   // function definition
+}

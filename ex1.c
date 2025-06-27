114e91659315e96388e9018681d6eb83ecf38a91
Bhuwin <149753612+Bhuwinrag@users.noreply.github.com>
2025-06-27 10:25:40 +0530

    Rotate the last number

--- ex1.c
+++ ex1.c
@@ -1,10 +1,34 @@
+/*Write a C program to rotate the last number*/
 #include<stdio.h>
+
+int scan();
+int largest_10_power(int);
+int l_rotate(int);
+
 int main(){
+    int result= l_rotate(scan());
+    printf("%d", result);
+}
+
+int l_rotate(int num){
+    int den = largest_10_power(num);
+    int left= num/den;
+    int right= num%den;
+    int new_num = right*10 + left;
+    return new_num;
+}
+
+int largest_10_power(int num){
+    int den=1;
+    while ((num/den)>9)
+    {
+        den*10;
+    }
+    return den;
+}
+
+int scan(){
     int num;
     scanf("%d", &num);
-    int start=1;
-    do{
-        printf("%d" , start);
-        start++;
-    }while (start<=num);
+    return num;
 }

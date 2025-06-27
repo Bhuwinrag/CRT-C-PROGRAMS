46baa40b973e2c7f03ca7eccfc69c5d6ffe942eb
Bhuwin <149753612+Bhuwinrag@users.noreply.github.com>
2025-06-27 10:26:35 +0530

    Circular prime

--- ex2.c
+++ ex2.c
@@ -1,8 +1,61 @@
+/*Write a C program to check the circular prime or not*/
 #include<stdio.h>
+#include<math.h>
+
+int scan();
+int largest_10_power(int);
+int is_prime(int);
+int l_rotate(int);
+int circular_prime(int);
+
 int main(){
-    goto greet_branch;
-    printf("Hello World");
-    greet_branch:
-        printf("CSE");
-    printf("\n PSCMR");
+    int result= circular_prime(scan());
+    printf(result? "Circular": "Not Circular");
+}
+
+int circular_prime(int num){
+    int nod = log10(num)+1;
+    for(int itr=1;itr<=num;itr++){
+        if(is_prime(num)){
+            num=l_rotate(num);
+        }
+        else{
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int is_prime(int num){
+    if(num<=1) return 0;
+    if(num%2==0) return num==2;
+    for(int itr=3; itr<=sqrt(num); itr+=2){
+        if(num%itr==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int l_rotate(int num){
+    int den = largest_10_power(num);
+    int left= num/den;
+    int right= num%den;
+    int new_num = right *10 + left;
+    return new_num;
+}
+
+int largest_10_power(int num){
+    int den=1;
+    while ((num/den)>9)
+    {
+        den *=10;
+    }
+    return den;
+}
+
+int scan(){
+    int num;
+    scanf("%d", &num);
+    return num;
 }
